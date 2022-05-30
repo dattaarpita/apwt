da286a661e07a2c56ffd6127d508f2e8fc7ea21f
dattaarpita <19-41608-3@student.aiub.edu>
2022-05-31 01:20:42 +0600

    welcome message updated

--- APWT_TASK_1/resources/views/Home.blade.php
+++ APWT_TASK_1/resources/views/Home.blade.php
@@ -2,5 +2,5 @@
 @section('content')
 <br>
 <h1>This is online pharmaceuticals</h1>
-<p>You can find many types of medicine here.</p>
+<p>You can find many types of medicine here.Please stay with us!</p>
 @endsection
